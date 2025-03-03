d604a8183247f86de2c87da0aac56fea4f559e55
BoBynou <90771596+BoBynou@users.noreply.github.com>
2025-03-03 09:41:46 +0100

    cc
    # Conflicts:
    #	src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,46 @@
-#include <Arduino.h>
-#include "esp_bt_main.h"
-#include "esp_bt_device.h"
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
-#include <BLE2904.h>
-#include <Wire.h>
-#include <Adafruit_DRV2605.h>
+#include <esp_now.h>
+#include <WiFi.h>
+/// AJOUTER L'HEURE POUR VIBRER ET AUSSI LORS DE L'APPUIE SUR UN BOUTON ARRETER DE FAIRE VIBRER
+#define LED_PIN 1
+#define LED_RED_PIN 2
+#define LED_GREEN_PIN 3
+#define LED_BLUE_PIN 4
 #include "time.h"
-
-#define LED_PIN 2
-#define BUTTON_PIN 1
-#define SERVICE_UUID "12345678-1234-1234-1234-123456789abc"
-#define CHARACTERISTIC_UUID "12345678-1234-1234-1234-123456789abc"
-
-#define SDA 5
-#define SCL 6
-
-Adafruit_DRV2605 drv;
-BLECharacteristic *pCharacteristic;
-
 String dataValue = "OFF";
-
-class MyServerCallbacks : public BLEServerCallbacks
+typedef struct struct_message
 {
-public:
-  void onConnect(BLEServer *pServer)
-  {
-    Serial.println("Client connected");
-    Serial.flush(); // Ensure the message is completely sent
-  }
+  char a[32];
+} struct_message;
 
-  void onDisconnect(BLEServer *pServer)
-  {
-    Serial.println("Client disconnected, restarting ESP32...");
-    Serial.flush(); // Ensure the message is completely sent
-    delay(1000);    // Wait a moment before restarting
-    ESP.restart();
-  }
-};
+struct_message incomingMessage;
 
-class MyCallbacks : public BLECharacteristicCallbacks
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
 {
-  void onWrite(BLECharacteristic *pCharacteristic)
+  memcpy(&incomingMessage, incomingData, sizeof(incomingMessage));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("Message: ");
+  Serial.println(incomingMessage.a);
+
+  if (strcmp(incomingMessage.a, "LED_ON") == 0)
   {
-    std::string value = pCharacteristic->getValue();
-    if (value == "ON")
-    {
-      digitalWrite(LED_PIN, HIGH);
-      dataValue = "ON";
-    
-    }
-    else if (value == "OFF")
-    {
-      digitalWrite(LED_PIN, LOW);
-      dataValue = "OFF";
-    }
+    digitalWrite(LED_PIN, HIGH);
   }
-};
+  else if (strcmp(incomingMessage.a, "LED_OFF") == 0)
+  {
+    digitalWrite(LED_PIN, LOW);
+  }
+  else if (strcmp(incomingMessage.a, "LED_FRANCE") == 0)
+  {
+    int red = random(0, 256);
+    int green = random(0, 256);
+    int blue = random(0, 256);
+
+    analogWrite(LED_RED_PIN, red);
+    analogWrite(LED_GREEN_PIN, green);
+    analogWrite(LED_BLUE_PIN, blue);
+  }
+}
 
 void setTimeFromPillulier(int yr, int month, int mday, int hr, int minute, int sec, int isDst) {
   Serial.printf("Paramètrage de l'ESP-32 à l'heure du pillulier'...");
@@ -80,36 +62,23 @@ void setup()
 {
   Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  BLEDevice::init("ESP32_JEREM");
-  BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  pCharacteristic = pService->createCharacteristic(
-      CHARACTERISTIC_UUID,
-      BLECharacteristic::PROPERTY_READ |
-          BLECharacteristic::PROPERTY_WRITE |
-          BLECharacteristic::PROPERTY_NOTIFY);
-  BLE2904 *p2904Descriptor = new BLE2904();
-  p2904Descriptor->setFormat(BLE2904::FORMAT_UTF8);
-  pCharacteristic->addDescriptor(p2904Descriptor);
-  pCharacteristic->setCallbacks(new MyCallbacks());
-  pService->start();
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->start();
-  Serial.println("Waiting for a client connection to notify...");
-  Wire.begin(SDA, SCL);
-  if(!drv.begin()) {
-    Serial.println("Impossible de trouver le DRV2605L");
-    while(1);
+  pinMode(LED_RED_PIN, OUTPUT);
+  pinMode(LED_GREEN_PIN, OUTPUT);
+  pinMode(LED_BLUE_PIN, OUTPUT);
+  WiFi.mode(WIFI_STA);
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.println("Error initializing ESP-NOW");
+    return;
   }
+  esp_now_register_recv_cb(OnDataRecv);
   drv.selectLibrary(1);
   drv.setMode(DRV2605_MODE_INTTRIG);
 }
 
 void loop()
 {
-
+  digitalWrite(LED_PIN, LOW);
   if (digitalRead(BUTTON_PIN) == LOW)
   {
     pCharacteristic->setValue("OFF");
